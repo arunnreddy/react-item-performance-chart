20a314e048bd1269121b62e547ecc09d71096f9d
biswajitmaity2019 <biswajitmaity889@gmail.com>
2021-12-21 10:45:40 +0530

    1st commit for add redux provider store value in items.stories.tsx inside stories on 21/12/2021

--- src/stories/Items.stories.tsx
+++ src/stories/Items.stories.tsx
@@ -1,5 +1,7 @@
 import { Meta, Story } from "@storybook/react";
 import Items from "../Containers/Items";
+import { Provider } from "react-redux";
+import store from "../Redux/Store";
 
 export default {
     title: "Item List",
@@ -7,7 +9,9 @@ export default {
 
 const ItemFuc: Story = () => (
     <div>
+        <Provider store={store}>
         <Items />
+        </Provider>
     </div>
 );
 
